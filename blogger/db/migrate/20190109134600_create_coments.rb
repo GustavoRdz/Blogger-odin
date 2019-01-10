0cf3bca7ff96c0c02db8b99c77b7ecf0b81d214d
Gustavo Rdz <gustavo.rodriguezml@gmail.com>
2019-01-09 19:03:38 -0600

    Finished I2 part: Adding comments

---
+++ blogger/db/migrate/20190109134600_create_coments.rb
@@ -0,0 +1,11 @@
+class CreateComents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :coments do |t|
+      t.string :author_name
+      t.text :body
+      t.references :article, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
